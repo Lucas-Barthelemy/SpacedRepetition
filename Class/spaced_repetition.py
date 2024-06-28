684ee06bfaf9388572f1cd8bb910a11163524523
LucasBart <lucasbart@hotmail.fr>
2024-06-28 10:52:38 +0200

    create the main interface
    Date, Course and Button

--- Class/spaced_repetition.py
+++ Class/spaced_repetition.py
@@ -1,16 +1,83 @@
 import customtkinter as ctk
+import datetime
 
 class SpacedRepetition:
     def __init__(self, root):
         self.root = root
-        self.root.title('Spaced repetition')
+        self.root.title('Spaced Repetition')
 
-        # Create a frame with padding
-        self.mainframe = ctk.CTkFrame(self.root)
-        self.mainframe.pack(padx=12, pady=12)
+        # Date
+        self.date_frame = ctk.CTkFrame(self.root)
+        self.date_frame.pack(
+            side="top",
+            padx=20,
+            pady=20,
+            fill="x"
+        )
+        
+        today = datetime.date.today()
+        date_label = ctk.CTkLabel(
+            self.date_frame,
+            text=today.strftime("%B %d, %Y")
+        )
+        date_label.pack(side="left")
+        
+        # Main
+        self.main_frame = ctk.CTkFrame(self.root)
+        self.main_frame.pack(
+            side="bottom",
+            padx=20,
+            pady=20,
+            fill="both",
+            expand=True
+        )
 
-        # Create a label
-        ctk.CTkLabel(self.mainframe, text="Hello, Tkinter!").pack()
+        
+        # Courses
+        self.courses_frame = ctk.CTkFrame(self.main_frame)
+        self.courses_frame.pack(
+            side="left",
+            padx=20,
+            pady=20,
+            fill="both",
+            expand=True
+        )
+        
+        # Buttons
+        self.buttons_frame = ctk.CTkFrame(self.main_frame)
+        self.buttons_frame.pack(
+            side="right",
+            padx=20,
+            pady=20,
+            fill="both",
+            expand=True
+        )
 
-        # Create a button
-        ctk.CTkButton(self.mainframe, text="Quit", command=self.root.destroy).pack()
+        add_button = ctk.CTkButton(
+            self.buttons_frame,
+            text="Add Course",
+            command=self.add_course
+        )
+        add_button.pack(
+            side="top",
+            pady=10
+        )
+        
+        quit_button = ctk.CTkButton(
+            self.buttons_frame,
+            text="Quit",
+            command=self.quit
+        )
+        quit_button.pack(
+            side="top",
+            pady=10
+        )
+        
+        self.main_frame.grid_columnconfigure(0, weight=8)
+        self.main_frame.grid_columnconfigure(1, weight=2)
+
+    def add_course(self):
+        pass
+    
+    def quit(self):
+        self.root.destroy()
